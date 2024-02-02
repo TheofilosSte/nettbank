069664474f31279801f5da06776a1077d4e03f85
Theofilos Stefanidis <s366447@oslomet.no>
2024-02-02 11:44:32 +0100

    test

---
+++ Nettbank/Nettbank/src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -0,0 +1,124 @@
+package oslomet.testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.BankController;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Models.Transaksjon;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestBankController {
+
+    @InjectMocks
+    // denne skal testes
+    private BankController bankController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private Sikkerhet sjekk;
+
+    @Test
+    public void hentTransakjonerTest(){
+        //arrange
+        Transaksjon enTransaksjon = new Transaksjon(3343434, "324334554545", 1000,
+                "03.05.2023", "hei", "sdsd", "1099000" );
+        when (sjekk.loggetInn()).thenReturn("3343434");
+        when(repository.hentTransaksjoner(anyString(), anyString(), anyString(), )).thenReturn(enTransaksjon);
+
+        //act
+        Transaksjon fullfort = bankController.hentTransaksjoner();
+        //asert
+        assertEquals(enTransaksjon, fullfort);
+
+    }
+    @Test
+    public void hentIkkeTransaksjonTest(){
+        //arrange
+        when(sjekk.)
+    }
+    @Test
+    public void hentKundeInfo_loggetInn() {
+
+        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
+
+        // act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertEquals(enKunde, resultat);
+    }
+
+    @Test
+    public void hentKundeInfo_IkkeloggetInn() {
+
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void hentKonti_LoggetInn()  {
+        // arrange
+        List<Konto> konti = new ArrayList<>();
+        Konto konto1 = new Konto("105010123456", "01010110523",
+                720, "Lønnskonto", "NOK", null);
+        Konto konto2 = new Konto("105010123456", "12345678901",
+                1000, "Lønnskonto", "NOK", null);
+        konti.add(konto1);
+        konti.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKonti(anyString())).thenReturn(konti);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertEquals(konti, resultat);
+    }
+
+    @Test
+    public void hentKonti_IkkeLoggetInn()  {
+        // arrange
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertNull(resultat);
+    }
+}
+
